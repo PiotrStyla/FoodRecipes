b15e576c04f6ddc7d6920b619ce07161d2f964df
Admin <p.styla@gmail.com>
2019-09-11 20:45:24 +0200

    responces modeling

---
+++ app/src/main/java/pl/aplikacje/foodrecipes/requests/responses/RecipeReponse.java
@@ -0,0 +1,31 @@
+package pl.aplikacje.foodrecipes.requests.responses;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import pl.aplikacje.foodrecipes.models.Recipe;
+
+public class RecipeReponse {
+
+    @SerializedName("recipe")
+    @Expose
+    private Recipe recipe;
+    public Recipe getRecipe(){
+        return recipe;
+    }
+
+    @Override
+    public String toString() {
+        return "RecipeReponse{" +
+                "recipe=" + recipe +
+                '}';
+    }
+}
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/pl/aplikacje/foodrecipes/requests/responses/RecipeSearchResponse.java
@@ -0,0 +1,43 @@
+package pl.aplikacje.foodrecipes.requests.responses;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+import pl.aplikacje.foodrecipes.models.Recipe;
+
+public class RecipeSearchResponse {
+
+
+
+
+    @SerializedName("count")
+    @Expose
+    private int count;
+
+
+    @SerializedName("recipes")
+    @Expose
+    private List<Recipe> recipes;
+
+    public int getCount() {
+        return count;
+    }
+
+    public List<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    @Override
+    public String toString() {
+        return "RecipeSearchResponse{" +
+                "count=" + count +
+                ", recipes=" + recipes +
+                '}';
+    }
+}
+
+
+
+
