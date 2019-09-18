6902216ab24dcf5c172967d194322a44d61ff2b9
Admin <p.styla@gmail.com>
2019-09-18 21:06:19 +0200

    responces modeling - view model

--- app/src/main/java/pl/aplikacje/foodrecipes/RecipeListActivity.java
+++ app/src/main/java/pl/aplikacje/foodrecipes/RecipeListActivity.java
@@ -1,25 +1,30 @@
 package pl.aplikacje.foodrecipes;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.util.Log;
-import android.view.View;
+        import android.view.View;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+        import java.io.IOException;
+        import java.util.ArrayList;
+        import java.util.List;
 
-import pl.aplikacje.foodrecipes.models.Recipe;
-import pl.aplikacje.foodrecipes.requests.RecipeApi;
-import pl.aplikacje.foodrecipes.requests.ServiceGenerator;
-import pl.aplikacje.foodrecipes.requests.responses.RecipeSearchResponse;
-import pl.aplikacje.foodrecipes.util.Constants;
+        import pl.aplikacje.foodrecipes.models.Recipe;
+        import pl.aplikacje.foodrecipes.requests.RecipeApi;
+        import pl.aplikacje.foodrecipes.requests.ServiceGenerator;
+        import pl.aplikacje.foodrecipes.requests.responses.RecipeSearchResponse;
+        import pl.aplikacje.foodrecipes.util.Constants;
+import pl.aplikacje.foodrecipes.viewModels.RecipeListViewModel;
 import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+        import retrofit2.Callback;
+        import retrofit2.Response;
 
 public class RecipeListActivity extends BaseActivity {
 
     private static final String TAG = "RecipeListActivity";
+    private RecipeListViewModel mRecipeListViewModel;
 
 
     @Override
@@ -27,14 +32,20 @@ public class RecipeListActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_list);
 
-        findViewById(R.id.test).setOnClickListener(new View.OnClickListener() {
+        mRecipeListViewModel = ViewModelProviders.of(this).get(RecipeListViewModel.class);
+
+        subscribeObservers();
+
+    }
+
+
+    private void subscribeObservers(){
+        mRecipeListViewModel.getRecipes().observe(this, new Observer<List<Recipe>>() {
             @Override
-            public void onClick(View v) {
-                testRetrofitRequest();
+            public void onChanged(@Nullable List<Recipe> recipes) {
 
             }
         });
-
     }
 
     private void testRetrofitRequest() {
