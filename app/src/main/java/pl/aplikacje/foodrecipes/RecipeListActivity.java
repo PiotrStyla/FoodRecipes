171d279064c4a6e436e452401db157b089eb81d2
Admin <p.styla@gmail.com>
2019-10-11 21:28:38 +0200

    search added

--- app/src/main/java/pl/aplikacje/foodrecipes/RecipeListActivity.java
+++ app/src/main/java/pl/aplikacje/foodrecipes/RecipeListActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
 import android.view.View;
 
@@ -44,7 +45,7 @@ public class RecipeListActivity extends BaseActivity implements OnRecipeListener
 
         initRecyclerView();
         subscribeObservers();
-        testRetrofitRequest();
+        initSearchView();
 
 
     }
@@ -74,12 +75,21 @@ public class RecipeListActivity extends BaseActivity implements OnRecipeListener
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
-    private void searchRecipesApi(String querry, int pageNumber) {
-        mRecipeListViewModel.searchRecipesApi(querry, pageNumber);
-    }
 
-    private void testRetrofitRequest() {
-        searchRecipesApi("Chicken Breast", 1);
+    private void initSearchView(){
+        final SearchView searchView = findViewById(R.id.searchView);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                mRecipeListViewModel.searchRecipesApi(s, 1);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
 
     }
 
